6d60f09504c31e2731cd03bd40e2decde7eac4b3
molmatt <mol.matthew.j@gmail.com>
2019-02-09 15:24:06 -0600

    initial deploy

---
+++ GetFood.py
@@ -0,0 +1,52 @@
+from flask import render_template, Flask, request
+import googlemaps as gmaps
+import pandas as pd
+
+# Initialize app
+app = Flask(__name__, static_url_path='/static')
+
+# Starting page with form
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('index.html')
+
+# Restaurant suggestion page
+@app.route('/results', methods=['GET', 'POST'])
+def results():
+    specs = request.form.to_dict()
+    result = FoodSpinner(address = specs['address'],
+                style = specs['style'],
+                price = int(specs['price']),
+                rating = float(specs['rating']),
+                distance = specs['distance'],
+                choices = int(specs['choices']))
+    return render_template('results.html', nam=result['Name'], rat=result['Rating'], ratc=result['Rating Count'], pri=result['Price'])
+
+
+
+def FoodSpinner(address, style=None, price = 2, rating = 4, distance=1000, choices=1, nowOpen=True):
+    gmapi = gmaps.Client(key='AIzaSyC85HHHj3JTeehfiMAYazam2F5OZjwYTYc')
+    addy = gmapi.geocode(address)
+    latlon = addy[0]['geometry']['location']
+    srch = gmapi.places_nearby(location=latlon, radius=distance, type='restaurant', keyword=style)
+    names = []
+    prices = []
+    ratings = []
+    numRatings = []
+    for i in srch['results']:
+        names.append(i['name'])
+        prices.append(i['price_level']) if 'price_level' in i.keys() else prices.append(np.nan)
+        ratings.append(i['rating'])
+        numRatings.append(i['user_ratings_total'])
+    df = pd.DataFrame({
+        'Name' : names,
+        'Price' : prices,
+        'Rating' : ratings,
+        'Rating Count' : numRatings
+    })
+    print(len(df.index), 'matching this cuisine in range')
+    df.drop(df.index[df['Price'] > price], inplace=True)
+    print(len(df.index), 'meeting the price cutoff')
+    df.drop(df.index[df['Rating'] < rating], inplace=True)
+    print(len(df.index), 'meeting the rating cutoff')
+    return(df.sample(choices).reset_index().iloc[:,1:])
